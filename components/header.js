dab21e0843f55a1d02a48f624ba843bc4d0e6c3b
willp11 <williampage11@yahoo.co.uk>
2022-08-10 13:23:32 +0700

    changed cart icon to use tailwind group to change fill on hover

--- components/header.js
+++ components/header.js
@@ -1,19 +1,13 @@
 import ShoppingCartIcon from "./icons/shoppingCartIcon";
-import { useState } from "react";
 
 export default function Header() {
-
-    const [fill, setFill] = useState("fill-gray-600");
 
     return (
         <div className="h-[80px] px-6 py-2 border-b-2 border-gray-200 flex items-center justify-center">
             <div className="w-full max-w-[64rem] flex justify-between items-center">
                 <p className="text-3xl tracking-tight font-extrabold">MyShop</p>
-                <div className="flex items-center cursor-pointer"
-                    onMouseEnter={()=>setFill("fill-black")}
-                    onMouseLeave={()=>setFill("fill-gray-600")}
-                >
-                    <ShoppingCartIcon fill={fill} />
+                <div className="flex items-center cursor-pointer group">
+                    <ShoppingCartIcon />
                     <p className="text-lg">$0.00</p>
                     <p className="text-gray-500 pl-1">(0)</p>
                 </div>
