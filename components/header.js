7b6f67a0c0187b277d86a4961525b06b9db58724
willp11 <williampage11@yahoo.co.uk>
2022-08-11 11:30:53 +0700

    created basic cart page

--- components/header.js
+++ components/header.js
@@ -1,15 +1,20 @@
 import ShoppingCartIcon from "./icons/shoppingCartIcon";
 import { useCart } from "../hooks/useCart";
+import { useRouter } from "next/router";
 
 export default function Header() {
 
+    const router = useRouter();
     const {cart} = useCart();
 
     return (
         <div className="w-full h-[80px] px-6 py-2 border-b-2 border-gray-200 flex items-center justify-center">
             <div className="w-full max-w-[64rem] flex justify-between items-center">
-                <p className="text-3xl tracking-tight font-extrabold">MyShop</p>
-                <div className="flex items-center cursor-pointer group">
+                <p className="text-3xl tracking-tight font-extrabold cursor-pointer" onClick={()=>router.push("/")}>MyShop</p>
+                <div 
+                    className="flex items-center cursor-pointer group"
+                    onClick={()=>router.push("/cart")}
+                >
                     <ShoppingCartIcon />
                     <p className="text-lg">${cart.value.toFixed(2)}</p>
                     <p className="text-gray-500 pl-1">({cart.total_qty})</p>
--- pages/cart.js
+++ pages/cart.js
@@ -1,14 +1,53 @@
 import Layout from "../components/layout";
 import Head from "next/head";
+import { useCart } from "../hooks/useCart";
+import Image from "next/image";
 
 export default function Cart() {
+
+    const {cart, removeItem} = useCart();
+
+    let cartItems = Object.keys(cart.items).map((key)=>{
+        console.log(key)
+        let item = cart.items[key];
+        return (
+            <div className="w-full mx-1 my-4 px-2 py-4 border border-gray-200 rounded-md shadow-md flex justify-start items-center overflow-x-auto">
+                <div className="h-[100px] min-w-[100px]">
+                    <Image
+                        src={item.image}
+                        height={100}
+                        width={100}
+                        alt={item.name}
+                    />
+                </div>
+                <div>
+                    <p className="text-lg font-semibold truncate">{item.name}</p>
+                    <div className="flex pt-2">
+                        <p>- {item.quantity} +</p>
+                        <p className="ml-8">x ${item.price}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    })
+
     return (
-        <Layout cart={[]}>
+        <Layout>
             <Head>
-                <title>Your Cart</title>
+                <title>MyShop Cart</title>
             </Head>
             <div className="w-full min-h-[calc(100vh-80px)] bg-gray-50 py-4 px-2">
-                
+                <div className="w-full max-w-[64rem] mx-auto flex flex-row flex-wrap justify-center items-start">
+                    <div className="w-full bg-white border border-gray-300 rounded shadow p-4">
+                        <h1 className="text-left">Your Cart</h1>
+                        <div className="flex items-center ml-2">
+                            <h2 className="text-lg font-semibold">{cart.total_qty} {cart.total_qty === 1 ? "item" : "items"}</h2>
+                            <p className="text-base text-gray-500 ml-4 cursor-pointer hover:text-black">(Clear All)</p>
+                        </div>
+                        {cartItems}
+                        <h2 className="text-lg font-semibold ml-2">Total: ${cart.value}</h2>
+                    </div>
+                </div>
             </div>
         </Layout>
     )
