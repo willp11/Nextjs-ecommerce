f2ec8d183610ab473998122cc625e2f9892534de
willp11 <williampage11@yahoo.co.uk>
2022-08-12 09:29:30 +0700

    fixed SSR hydration issue, fixed bug incorrect message removing item when load app with items already in cart

--- components/header.js
+++ components/header.js
@@ -13,43 +13,36 @@ export default function Header() {
     const [isSSR, setIsSSR] = useState(true);
     const [message, setMessage] = useMessage();
     const oldValueRef = useRef(0)
-    const firstRun = useRef(true);
 
     useEffect(() => {
         setIsSSR(false);
+        oldValueRef.current = cart.value;
     }, []);
 
     useEffect(()=>{
-        if (firstRun.current) {
-            firstRun.current = false;
-            return;
-        } else {
-            if (cart.value > oldValueRef.current) {
-                setMessage("Item added to cart");
-                oldValueRef.current = cart.value;
-            } else if (cart.value < oldValueRef.current) {
-                setMessage("Item removed from cart");
-                oldValueRef.current = cart.value;
-            }
+        if (cart.value > oldValueRef.current && !isSSR) {
+            setMessage("Item added to cart");
+            oldValueRef.current = cart.value;
+        } else if (cart.value < oldValueRef.current && !isSSR) {
+            setMessage("Item removed from cart");
+            oldValueRef.current = cart.value;
         }
     }, [cart])
     
-    if (!isSSR) {
-        return (
-            <div className="w-full h-[80px] px-6 py-2 border-b-2 border-gray-200 flex items-center justify-center">
-                <div className="w-full max-w-[64rem] flex justify-between items-center">
-                    <p className="text-3xl tracking-tight font-extrabold cursor-pointer" onClick={()=>router.push("/")}>MyShop</p>
-                    <div 
-                        className="flex items-center cursor-pointer group"
-                        onClick={()=>router.push("/cart")}
-                    >
-                        <ShoppingCartIcon className="h-[28px] w-[28px] cursor-pointer transition ease-in-out duration-300 fill-gray-600 group-hover:fill-black" />
-                        <p className="text-lg ml-1">${cart.value.toFixed(2)}</p>
-                        <p className="text-gray-500 pl-1">({cart.total_qty})</p>
-                    </div>
+    return (
+        <div className="w-full h-[80px] px-6 py-2 border-b-2 border-gray-200 flex items-center justify-center">
+            <div className="w-full max-w-[64rem] flex justify-between items-center">
+                <p className="text-3xl tracking-tight font-extrabold cursor-pointer" onClick={()=>router.push("/")}>MyShop</p>
+                <div 
+                    className="flex items-center cursor-pointer group"
+                    onClick={()=>router.push("/cart")}
+                >
+                    <ShoppingCartIcon className="h-[28px] w-[28px] cursor-pointer transition ease-in-out duration-300 fill-gray-600 group-hover:fill-black" />
+                    <p className="text-lg ml-1">${!isSSR ? cart.value.toFixed(2) : null}</p>
+                    <p className="text-gray-500 pl-1">({!isSSR ? cart.total_qty : null})</p>
                 </div>
-                {message !== "" ? <UpdateCartMsg message={message} /> : null}
             </div>
-        )
-    } else return null;
+            {message !== "" ? <UpdateCartMsg message={message} /> : null}
+        </div>
+    )
 }
--- pages/cart.js
+++ pages/cart.js
@@ -18,9 +18,8 @@ export default function Cart() {
     })
 
     let checkoutCursor = "cursor-pointer";
-    if (cartItems.length === 0) checkoutCursor = "cursor-not-allowed"
+    if (cartItems.length === 0 && !isSSR) checkoutCursor = "cursor-not-allowed"
 
-    if (!isSSR) {
         return (
             <>
                 <Head>
@@ -31,14 +30,14 @@ export default function Cart() {
                         <div className="w-full bg-white border border-gray-300 rounded shadow p-4">
                             <h1 className="text-4xl tracking-tight font-extrabold my-2 text-left">Your Cart</h1>
                             <div className="flex items-center ml-2">
-                                <h2 className="text-lg font-semibold">{cart.total_qty} {cart.total_qty === 1 ? "item" : "items"}</h2>
+                                {!isSSR ? <h2 className="text-lg font-semibold">{cart.total_qty} {cart.total_qty === 1 ? "item" : "items"}</h2> : null}
                                 <p className="text-base text-gray-500 ml-4 cursor-pointer hover:text-black" onClick={clearCart}>(Clear All)</p>
                             </div>
-                            {cartItems}
-                            <h2 className="text-lg font-semibold ml-2 mt-2">Total: ${cart.value}</h2>
+                            {!isSSR ? cartItems : null}
+                            <h2 className="text-lg font-semibold ml-2 mt-2">Total: ${!isSSR ? cart.value : null}</h2>
                             <button 
                                 className={`ml-2 mt-2 px-2 py-1 rounded font-semibold bg-blue-500 text-white hover:bg-blue-700 transition ease-in-out duration-300 ${checkoutCursor}`}
-                                disabled={cartItems.length === 0}
+                                disabled = {!isSSR ? cartItems.length === 0 : false}
                             >
                                 Go to checkout
                             </button>
@@ -47,5 +46,4 @@ export default function Cart() {
                 </div>
             </>
         )
-    } else return null;
 }
